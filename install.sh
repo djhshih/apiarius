425251222e8a164213e5842c23101be60f739aee
David J. H. Shih <djh.shih@gmail.com>
2015-09-17 23:23:16 -0400

    Fixed installation errors.

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 set -o errexit
 set -o nounset
 
-DESTDIR={$DESTDIR:-/usr/local}
+DESTDIR=${DESTDIR:-/usr/local}
 
 basepath=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # install mella after updating basepath
-sed "s|basepath=.*\basepath=$basepath|" $basepath/bin/mella > $DESTDIR/bin/mella
+sed "s|basepath=.*|basepath=$basepath|" $basepath/bin/mella > $DESTDIR/bin/mella
 chmod +x $DESTDIR/bin/mella
 
